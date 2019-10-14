70b487239e4963126020d5d2353f4d3186a682c7
wkw307 <wkw307@gmail.com>
2019-10-14 09:27:11 +0800

    calc facet weight

--- src/facet-tree-ng.ts
+++ src/facet-tree-ng.ts
@@ -20,7 +20,7 @@ interface FacetData {
     type: string;
     // true: 子节点是分面 false: 子节点是碎片
     containChildrenFacet: boolean;
-    children: AssembleData[] | FacetData[];
+    children: (AssembleData | FacetData)[];
 }
 
 interface TreeData {
@@ -31,6 +31,23 @@ interface TreeData {
     domainId: number;
     childrenNumber: number;
     children: FacetData[];
+}
+
+/**
+ * 计算分面权重
+ * @param facetData 分面数据 
+ */
+function calcWeight(facetData: FacetData): number {
+    if (facetData.containChildrenFacet) {
+        return 100 * facetData.childrenNumber + facetData.children.reduce((acc: number, curr: FacetData) => acc + calcWeight(curr), 0);
+    }
+    return facetData.children.reduce((acc: number, curr: AssembleData): number => {
+        if (curr.flag === 'fragment') {
+            return acc + 1;
+        } else {
+            return acc + 10;
+        }
+    }, 0);
 }
 
 // 分面字体大小范围
@@ -45,5 +62,68 @@ export function drawTree(data: TreeData, dom: HTMLObjectElement): void {
     // 一级分面数量
     const firstLayerNumber: number = data.childrenNumber;
 
+    // 判断是否视窗过小，需要折叠分面
+    const foldFlag = 0.8 * width < (14 * firstLayerNumber - 4) ? true : false;
+
+    // 计算分面权重
+    const firstLayerMap = [];
+    for (const facet of data.children) {
+        firstLayerMap.push({
+            facetId: facet.facetId,
+            value: calcWeight(facet),
+            containChildrenFacet: facet.containChildrenFacet,
+        });
+    }
+
+    // 根据权重排序
+    firstLayerMap.sort((a, b) => a.value - b.value);
+
+    // 视窗实在太小需要支持拖动
+    let dragflag = false;
+
+    let firstLayerTmp = data.children;
+
+    // 如果需要折叠
+    if (foldFlag) {
+        // 可容纳最多一级分面数
+        const maxFirstLayerNumber = Math.floor((width * 0.8 + 4) / 14);
+        // 权重大于100的分面（约为有二级分面的一级分面）
+        const firstLayerNumberWithSecondLayer = firstLayerMap.filter(x => x.value > 100).length;
+        // 剩余可折叠分面
+        const firstLayerNumberWithoutSecondLayer = firstLayerNumber - firstLayerNumberWithSecondLayer;
+
+        // 用来存放要折叠的一级分面facetId
+        const foldFacetIds = [];
+
+        if (maxFirstLayerNumber < firstLayerNumberWithSecondLayer + 1) {
+            // 最可容纳一级分面 < 有二级分面的一级分面数，只将可折叠的一级分面全部折叠
+            dragflag = true;
+            foldFacetIds.concat(firstLayerMap.filter(x => x.value < 100).map(x => x.facetId));
+        } else {
+            // 否则折叠部分
+            foldFacetIds.concat(
+                firstLayerMap.slice(maxFirstLayerNumber - 1 > 0 ? maxFirstLayerNumber - 1 : 0)
+                    .map(x => x.facetId)
+            );
+        }
+
+        if (foldFacetIds.length) {
+            const tmp: FacetData[] = [];
+            tmp.concat(firstLayerTmp.filter(x => foldFacetIds.indexOf(x.facetId) === -1));
+            const facetTmp: FacetData = {
+                facetId: -1,
+                facetName: '其他分面',
+                facetLayer: -1,
+                parentFacetId: 0,
+                topicId: data.topicId,
+                childrenNumber: foldFacetIds.length,
+                type: 'branch',
+                containChildrenFacet: true,
+                children: firstLayerTmp.filter(x => foldFacetIds.indexOf(x.facetId) !== -1),
+            };
+            tmp.push(facetTmp);
+            firstLayerTmp = tmp;
+        }
+    }
     
 }
