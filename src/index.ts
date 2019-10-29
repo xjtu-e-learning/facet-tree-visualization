a4427474c4fc0e3303988edff8fe1a27c0596d88
wkw307 <wkw307@gmail.com>
2019-10-29 15:56:58 +0800

    add pie chart facet name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { buildTree } from './facet-tree-ng';
+import { buildTree, FacetData } from './facet-tree-ng';
 import { data } from './data';
 import * as d3 from 'd3';
 import { color } from 'd3';
@@ -47,20 +47,49 @@ canvas.append('g')
 
 treeData.facetChart.forEach(element => {
     // 饼图
-    // canvas.append('g')
-    // .attr('transform', element.transform)
-    // .selectAll('path')
-    // .data(d3.pie().value(1)(element.children as any))
-    // .enter()
-    // .append('path')
-    // .attr('d', d3.arc()
-    // .innerRadius(0)
-    // .outerRadius(20) as unknown as string)
-    // .attr('fill', element.color)
-    // .attr("stroke", "white")
-    // .attr("stroke-width", element.r / 10)
+    canvas.append('g')
+        .attr('transform', element.transform)
+        .selectAll('path')
+        .data(d3.pie().value(1)(element.children as any))
+        .enter()
+        .append('path')
+        .attr('d', d3.arc()
+        .innerRadius(0)
+        .outerRadius(element.r) as unknown as string)
+        .attr('fill', element.color)
+        .attr("stroke", "white")
+        .attr("stroke-width", element.r / 10);
+    const num = element.childrenNumber;
+    const angle = Math.PI / num;
+    console.log(angle)
+    canvas.append('g')
+        .selectAll('text')
+        .data(element.children)
+        .enter()
+        .append('text')
+        .text(d => (d as FacetData).facetName)
+        .attr('x', (d, i) => {
+            if (angle === Math.PI) {
+                if (element.cx >= svg.clientWidth / 2) {
+                    return element.cx + element.r;
+                }
+                return element.cx - element.r - 12 * (d as FacetData).facetName.length;
+            }
+            if (Math.sin(angle * (2 * i + 1)) < 0) {
+                return element.cx + element.r * Math.sin(angle * (2 * i + 1)) - 12 * (d as FacetData).facetName.length - 12;
+            } 
+            return element.cx + element.r * Math.sin(angle * (2 * i + 1)) + 12;
+        })
+        .attr('y', (d, i) => {
+            if (angle === Math.PI) {
+                return element.cy - element.r;
+            }
+            return element.cy - element.r * Math.cos(angle * (2 * i + 1));
+        })
+        .attr('fill', '#000')
+        .attr('font-size', '12px');
     // 力导向图
-    drawFacetForceLayout(element, svg);
+    // drawFacetForceLayout(element, svg);
 });
 
 const texts = canvas.append('g')
